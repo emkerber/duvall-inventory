15a92c2b10b7ae55626c165de9c898543823d8a2
Liz Kerber <emkerber@outlook.com>
2024-01-23 19:50:50 -0600

    Create necessary tables

--- database.sql
+++ database.sql
@@ -4,3 +4,17 @@ CREATE TABLE "user" (
     "password" VARCHAR (1000) NOT NULL,
     "auth_level" INTEGER DEFAULT 0
 );
+
+CREATE TABLE "question" (
+    "id" SERIAL PRIMARY KEY,
+    "verbiage" VARCHAR (300) NOT NULL,
+    "category" VARCHAR (20) NOT NULL,
+    "weight" INTEGER DEFAULT 1
+);
+
+CREATE TABLE "response" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INTEGER REFERENCES "user",
+    "question_id" INTEGER REFERENCES "question",
+    "answer" INTEGER
+);
