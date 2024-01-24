2428414b5c3a76f45d9bc7dab56f7acf86466571
Liz Kerber <emkerber@outlook.com>
2024-01-23 19:56:19 -0600

    Update About component

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,7 +9,8 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p>The Duvall Inventory is a yet-to-be-published and also very groundbreaking system for assessing and quantifying individuals' likability.</p>
+        <p>Users will be presented a series of questions, and after responding to each question, the user's likability - the degree to which the user's peers enjoy/admire/value the user - will be displayed as a number.</p>
       </div>
     </div>
   );
